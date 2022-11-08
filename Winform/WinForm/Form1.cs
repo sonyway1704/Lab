c5895b442806b9e63d19a5bb05614d3c6a1350ad
sonyway <baotoantrinh2002@gmail.com>
2022-11-08 22:39:07 +0700

    WinForm
    Login Form to Main Form

---
+++ Winform/WinForm/Form1.cs
@@ -0,0 +1,33 @@
+namespace WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MainForm f2 = new MainForm();
+            this.Hide();
+
+            f2.Show();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (DialogResult.Yes == MessageBox.Show("Are you sure you want to exit?", "Exit", MessageBoxButtons.YesNo, MessageBoxIcon.Question))
+            {
+                Application.Exit();
+            }
+
+        }
+    }
+}
---
+++ Winform/WinForm/Program.cs
@@ -0,0 +1,22 @@
+using System.Configuration;
+
+namespace WinForm
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        /// 
+        public static string strConn = "";
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            strConn = ConfigurationManager.ConnectionStrings["MyConn"].ConnectionString;
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+        }
+    }
+}
